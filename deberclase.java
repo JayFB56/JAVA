b195f4aa5f45852cf8d539dcf4dda000cf7ce11f
Iber Javier Caicedo Ortiz <123275867+JayFB56@users.noreply.github.com>
2024-11-19 09:57:26 -0500

    Inicios del poo en java

---
+++ deberclase.java
@@ -0,0 +1,33 @@
+/*Realizar un prgrama que clacule el area y perimetro del hexagono
+ * usando conceptros de objetos de la siguiente manera:
+ * 1. ingreso
+ * 2. perimetro
+ * 3. area
+ * 4. impresion */
+
+import java.util.Scanner;
+
+public class deberclase {
+	private Scanner input;
+	private int lado, apotema, area, perimetro;
+	
+	public void ingreso() {
+		input = new Scanner(System.in);
+		System.out.print("Ingrese uno de los lados del hexagono: ");
+		lado=input.nextInt();
+	}
+	public void perimetro() {
+		
+	}public void area() {
+		
+	}
+	public void impresion() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
